463b21214000cb77e0e495b00ab40e9ab3ac5716
Marwan Mohamed <mrawan10152011@gmail.com>
2026-05-07 18:52:39 +0300

    Making Responsive Design For Celebrate Page

--- src/app/done/page.tsx
+++ src/app/done/page.tsx
@@ -2,7 +2,7 @@ import { JSX } from "react";
 import { itemVariants, MotionDiv, MotionH1, MotionLink, MotionMain, parentVariants } from "../../exports/global-methods";
 import styles from "./page.module.css";
 
-export default async function Done({ searchParams }: {searchParams: Promise<{ [key: string]: string | string[] | undefined }>}): Promise<JSX.Element> {
+export default async function Done({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }): Promise<JSX.Element> {
     const params = await searchParams;
     const getName: (string | undefined) = params.name as (string | undefined);
     return (
@@ -20,6 +20,9 @@ export default async function Done({ searchParams }: {searchParams: Promise<{ [k
                     <MotionH1 className={styles.info} variants={itemVariants}>اتمني لحضرتك يوم سعيد</MotionH1>
                     <MotionH1 className={styles.info} variants={itemVariants}>دي هدية مني لحضرتك</MotionH1>
                     <MotionH1 className={styles.info} variants={itemVariants}>اتمني الهدية تعجب حضرتك</MotionH1>
+                    <MotionH1 className={styles.info} variants={itemVariants}>
+                        معلش هدية ع قد الايد بس الوقت كان قصير اوي عشان الحق اعملها
+                    </MotionH1>
                     <MotionH1 className={styles.info} variants={itemVariants}>المرسل: مروان</MotionH1>
                 </MotionDiv>
                 <MotionDiv className={styles.back_btn_div} variants={itemVariants}>
